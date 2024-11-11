1f96749abb54f2e8ce662f8644121bc247aae82f
LahcenBenAmar <lahcen.ben3@gmail.com>
2024-11-11 15:13:57 +0100

    Initial commit

---
+++ src/main/java/com/banking/restapiebankify/RestApiEbankifyApplication.java
@@ -0,0 +1,13 @@
+package com.banking.restapiebankify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApiEbankifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApiEbankifyApplication.class, args);
+	}
+
+}
